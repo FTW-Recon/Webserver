33b107532b4a7143e11e7e374aed822da6b7b0a9
Tarcísio Marinho de Oliveira <tarcisio.marinho@xpi.com.br>
2021-01-24 23:38:41 -0300

    first commit

---
+++ FTW-Webserver/FTW-Webserver/Controllers/StoreFile/StoreFileController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FTWWebserver.Controllers.StoreFile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreFileController : ControllerBase
+    {
+        // GET: api/<StoreFileController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StoreFileController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StoreFileController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StoreFileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StoreFileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FTW-Webserver/FTW-Webserver/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FTWWebserver
+{
+    public static class DependencyInjection 
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            //services.AddScoped<IServ1, Impl1>();
+            return services;
+        }
+    }
+}
