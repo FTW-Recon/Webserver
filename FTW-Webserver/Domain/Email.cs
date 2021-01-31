a23a08856ed65d91198101e8c394cae26ce6c725
Tarcísio Marinho de Oliveira <tarcisio.marinho@xpi.com.br>
2021-01-31 01:05:21 -0300

    adding other domains

---
+++ FTW-Webserver/Domain/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Email
+    {
+        public string Template = @"<h1> User: {{User}}";
+        public string Source { get; set; }
+        public IEnumerable<string> Destination { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ FTW-Webserver/Domain/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Event
+    {
+        public string SenderName { get; set; }
+        public DateTime Date { get; set; }
+        public string JsonData { get; set; }
+    }
+}
